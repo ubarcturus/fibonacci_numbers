7139f1e3ed080265e8a31314e3c49c8d8e48fd0e
kuki <ubarcturus@gmail.com>
2022-10-20 15:52:42 +0200

    add calculations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let nth = 6;
+    let fibonacci = calculate_nth_fibonacci(nth);
+    println!("{fibonacci} is the {nth}th fibonacci number.");
+}
+
+fn calculate_nth_fibonacci(nth: i32) -> u64 {
+    let phi = (1.0 + 5.0_f32.sqrt()) / 2.0;
+    let fibonacci_of_nth = (phi.powi(nth - 1) / 5.0_f32.sqrt()).round() as u64;
+    return fibonacci_of_nth;
 }
